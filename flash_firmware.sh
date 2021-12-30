0348045de9fa2b6b57e836f99fe5ed165e8bac64
Cackharot <cackharot@gmail.com>
2021-12-30 10:58:24 +0530

    custom firmware with mqtt,tls,spi,i2c,dht

--- flash_firmware.sh
+++ flash_firmware.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-esptool.py --port /dev/cu.usbserial-1420 write_flash -fm qio 0x00000 ./bin/nodemcu-release-13-modules-2021-05-03-14-53-14-float.bin
+# esptool.py --port /dev/cu.usbserial-1420 write_flash -fm qio 0x00000 ./bin/nodemcu-release-13-modules-2021-05-03-14-53-14-float.bin
+esptool.py --port /dev/cu.usbserial-1420 write_flash -fm qio 0x00000 ./bin/0x00000.bin
+esptool.py --port /dev/cu.usbserial-1420 write_flash -fm qio 0x10000 ./bin/0x10000.bin
